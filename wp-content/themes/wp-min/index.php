dfab8979a5075930677e4e4dc3f36a944a2bd2f2
Rafa Ortman <rafaortman@gmail.com>
2023-01-19 14:55:01 -0300

    Update index.php

--- wp-content/themes/wp-min/index.php
+++ wp-content/themes/wp-min/index.php
@@ -1,7 +1,11 @@
 <!-- index.php -->
 <?php get_header(); ?>
-<?php while(have_posts()) : the_post(); ?>
-    <?php the_title(); ?>
-    <?php the_content(); ?>
-<?php endwhile; ?>
+<main>
+    <?php while(have_posts()) : the_post(); ?>
+        <h2>
+            <?php the_title(); ?>
+        </h2>
+        <?php the_content(); ?>
+    <?php endwhile; ?>
+</main>    
 <?php get_footer(); ?>
